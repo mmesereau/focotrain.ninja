5a75148f1cb27ab5cb01838dc7b568dfe74abd6e
Michael Mesereau <mmesereau1@gmail.com>
2016-07-08 18:58:39 -0600

    modules

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 'use strict';
+
+var ejs = require('ejs');
+var express = require('express');
+var app = express();
+
 var names = ["Mike", "Jimmy", "Roy", "Tara", "Steven"];
-var random = Math.floor(Math.random() * 5)
+var random = Math.floor(Math.random() * 5);
+var greeting = "Trains are approaching... be on your toes.";
+var ninjaName = names[random];
+var liveOrDie = Math.floor(Math.random * 2);
+var counter = [];
+for (var i = 0; i < names.length; i++) {
+  counter.push({name: names[i], survived: 0, died: 0});
+}
+if (liveOrDie % 2) {
+  counter[names.indexOf(ninjaName)].died++;
+  //dead
+}
+else {
+  counter[names.indexOf(ninjaName)].survived++;
+  //alive
+}
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+app.get('/go', function(req, res) {
+  
+})
+
+app.listen(3000, function() {
+  console.log("Listening on 3000...");
+})
